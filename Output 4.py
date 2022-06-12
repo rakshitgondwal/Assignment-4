d1c9b27097aaf001728654acff395653aac43dce
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2022-06-12 20:22:28 +0530

    Update Output 4.py

--- Output 4.py
+++ Output 4.py
@@ -1,7 +1,7 @@
 Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 
-================= RESTART: D:/Manish/New folder (2)/Output 4.py ================
+================= RESTART: D:/Rakshit/New folder (2)/Output 4.py ================
 Enter your Marks: 85
 Your Grade is A
 Enter the Year:1700
