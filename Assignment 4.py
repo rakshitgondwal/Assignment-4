e2dc6143ad055559de32bc8fbce54a827819ab41
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2022-06-12 20:21:26 +0530

    Add files via upload

---
+++ Assignment 4.py
@@ -0,0 +1,42 @@
+#Question 1
+Marks=int(input("Enter your Marks: "))
+if Marks<25:
+    print("Your Grade is F")
+elif Marks>=25 and Marks<45:
+    print("Your Grade is E")
+elif Marks>=45 and Marks<50:
+    print("Your Grade is D")
+elif Marks>=50 and Marks<60:
+    print("Your Grade is C")
+elif Marks>=60 and Marks<80:
+    print("Your Grade is B")
+else:
+    print("Your Grade is A")
+
+#Question 2
+Year=int(input("Enter the Year:"))
+if Year%4==0 and Year%100==0 and Year%400==0:
+    print("This year is a leap year")
+else:
+    print("This year is not a leap year")
+
+#Question 3
+import random
+for i in range(1,11,1):
+    m=random.randrange(1,10,1)
+    n=random.randrange(1,10,1)
+    ans=int(input((f"Question {i}: {m}*{n} = ")))
+    if m*n==ans:
+        print("Right!")
+        continue
+    else:
+        print("Wrong. The answer is ",m*n)
+        continue
+
+#Question 4
+for i in range(200):
+    if i%5==2:
+        if i%6==3:
+            if i%7==2:
+                print(i)
+
---
+++ Output 4.py
@@ -0,0 +1,29 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: D:/Manish/New folder (2)/Output 4.py ================
+Enter your Marks: 85
+Your Grade is A
+Enter the Year:1700
+This year is not a leap year
+Question 1: 1*5 = 5
+Right!
+Question 2: 2*3 = 5
+Wrong. The answer is  6
+Question 3: 8*9 = 72
+Right!
+Question 4: 1*9 = 10
+Wrong. The answer is  9
+Question 5: 7*7 = 49
+Right!
+Question 6: 9*4 = 36
+Right!
+Question 7: 6*5 = 19
+Wrong. The answer is  30
+Question 8: 1*3 = 5
+Wrong. The answer is  3
+Question 9: 7*3 = 73
+Wrong. The answer is  21
+Question 10: 5*1 = 5
+Right!
+177
